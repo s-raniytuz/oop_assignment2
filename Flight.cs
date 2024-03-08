c729fecd3aa4306d2f24ab7881ce753902d89114
s-raniytuz <tuzsocial909@gmail.com>
2024-03-08 11:29:38 -0700

    added Flight and FlightManagement classes

---
+++ Flight.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment2
+{
+    internal class Flight
+    {
+        public string FlightNumber { get; set; }
+        public string Airline { get; set; }
+        public string DepartureAirport { get; set; }
+        public string ArrivalAirport { get; set; }
+        public string DayOfTheWeek { get; set; }
+        public string Time { get; set; }
+
+        public int Idk { get; set; }
+        public double Cost { get; set; }
+
+        public Flight(string flightNumber, string airline, string departureAirport, string arrivalAirport, string day, string time, int idk, double cost)
+        {
+            FlightNumber = flightNumber;
+            Airline = airline;
+            DepartureAirport = departureAirport;
+            ArrivalAirport = arrivalAirport;
+            DayOfTheWeek = day;
+            Time = time;
+            Idk = idk;
+            Cost = cost;
+        }
+
+        public string toString()
+        {
+            return $"Flight number: {FlightNumber}\nAirline: {Airline}\nDeparture Airport: {DepartureAirport}\nArrival Airport: {ArrivalAirport}\nDay: {DayOfTheWeek}\nTime: {Time}\nIdk: {Idk}\nCost: {Cost}\n";
+        }
+    }
+}
---
+++ FlightManagement.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment2
+{
+    internal class FlightManagement
+    {
+        public static List<Flight> FindFlights(List<Flight> flights, string departureAirport, string arrivalAirport, string day)
+        {
+            List<Flight> searchList = new List<Flight>();
+
+            foreach (Flight flight in flights)
+            {
+                if (flight.DepartureAirport == departureAirport && flight.ArrivalAirport == arrivalAirport && flight.DayOfTheWeek == day)
+                {
+                    searchList.Add(flight);
+                }
+            }
+
+            return searchList;
+        }
+
+    }
+}
