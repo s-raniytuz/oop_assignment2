2487d9dbbc6964f6e140d8356664aa81b067aeb4
s-raniytuz <tuzsocial909@gmail.com>
2024-03-08 21:25:33 -0700

    select flight

---
+++ SelectedFlight.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment2
+{
+    internal class SelectedFlight
+    {
+        public string FlightNumber {  get; set; }
+        public string Airline {  get; set; }
+        public string Day {  get; set; }
+        public string Time {  get; set; }
+        public double Cost {  get; set; }
+        public string Name { get; set; }
+        public string Citizenship {  get; set; }
+
+        public SelectedFlight()
+        {
+            FlightNumber = " ";
+            Airline = "";
+            Day = "";
+            Time = "";
+            Cost = 0;
+            Name = "";
+            Citizenship = "";
+        }
+    }
+}
